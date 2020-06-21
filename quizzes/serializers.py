c5b8479b1410c0f0839b1667b1ae691782542457
sepehr javid <javid.sepehr77@gmail.com>
2020-06-21 23:46:21 +0430

    add validation for quiz answer and question

--- quizzes/serializers.py
+++ quizzes/serializers.py
@@ -1,5 +1,10 @@
-from rest_framework import serializers
+import datetime
 
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from learn_online_backend.settings import QUESTION_TYPES, MULTIPLE_CHOICE, TRUE_OR_FALSE, DESCRIPTIVE
 from quizzes.models import Quiz, QuizAnswer
 
 
@@ -16,6 +21,33 @@ class QuizSerializer(serializers.ModelSerializer):
 
         read_only_fields = ("id",)
 
+    def validate_questions(self, questions):
+        if len(questions) == 0:
+            raise ValidationError("Questions cannot be empty")
+        for question in questions:
+            keys = question.keys()
+            if 'text' not in keys:
+                raise ValidationError("Text of the question is not included")
+            if 'type' not in keys:
+                raise ValidationError("Type of the question is not specified")
+            if question.get("type") not in QUESTION_TYPES.keys():
+                raise ValidationError("Invalid question type")
+            if QUESTION_TYPES[question.get("type")] == MULTIPLE_CHOICE:
+                if 'choices' not in keys:
+                    raise ValidationError("Choices of multiple choice question are not included")
+                for choice in question.get("choices"):
+                    if type(choice) != str:
+                        raise ValidationError("Choices must be string")
+        return questions
+
+    def validate(self, attrs):
+        now = datetime.datetime.now()
+        if now > attrs.get("start_date"):
+            raise ValidationError("Start date cannot be before current time")
+        if attrs.get("start_date") >= attrs.get("deadline"):
+            raise ValidationError("Deadline cannot be before or at the same time as start date")
+        return attrs
+
 
 class QuizAnswerSerializer(serializers.ModelSerializer):
     quiz = QuizSerializer(read_only=True)
@@ -30,3 +62,21 @@ class QuizAnswerSerializer(serializers.ModelSerializer):
             "answers",
         ]
         read_only_fields = ("id", "score", "sent_date")
+
+    def validate_answers(self, answers):
+        question_id = self.context.get("view").kwargs.get("pk")
+        questions = get_object_or_404(Quiz, id=question_id).questions
+        if len(answers) != len(questions):
+            raise ValidationError("number of Answers is not equal to number of questions")
+        for i in range(len(answers)):
+            if questions[i]["type"] == MULTIPLE_CHOICE:
+                if answers[i] not in questions[i]["choices"]:
+                    raise ValidationError("Invalid choice answer")
+                if type(answers[i]) != str:
+                    raise ValidationError("Choice answer must be string")
+            if questions[i]["type"] == TRUE_OR_FALSE and type(answers[i]) != bool:
+                raise ValidationError("True or false answer should be boolean")
+            if questions[i]["type"] == DESCRIPTIVE and type(answers[i]) != str:
+                raise ValidationError("Descriptive answer must be string")
+
+        return answers
